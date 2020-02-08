5b9638c6b720328374f4de75ee2bfa3c059926fa
TomaszKopacz <tomasz.kopacz@onet.pl>
2020-02-08 17:55:16 +0100

    Adapter refactor

--- app/src/main/java/com/tomaszkopacz/kawernaapp/ui/main/board/ScoresAdapter.kt
+++ app/src/main/java/com/tomaszkopacz/kawernaapp/ui/main/board/ScoresAdapter.kt
@@ -3,6 +3,7 @@ package com.tomaszkopacz.kawernaapp.ui.main.board
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.tomaszkopacz.kawernaapp.R
@@ -24,10 +25,7 @@ class ScoresAdapter : RecyclerView.Adapter<ScoresAdapter.ScoresViewHolder>() {
     override fun onBindViewHolder(holder: ScoresViewHolder, position: Int) {
         val score = scores[position]
 
-        holder.setDate(score.date)
         holder.setTotal(score.total())
-        holder.setPlace(score.place)
-        holder.setPlayersCount(score.playersCount)
     }
 
     fun loadScores(scoresToLoad: ArrayList<Score>) {
@@ -36,25 +34,21 @@ class ScoresAdapter : RecyclerView.Adapter<ScoresAdapter.ScoresViewHolder>() {
     }
 
     class ScoresViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        private var dateTextView = itemView.findViewById<TextView>(R.id.date)
         private var totalTextView = itemView.findViewById<TextView>(R.id.total)
-        private var placeTextView = itemView.findViewById<TextView>(R.id.place)
-//        private var playersNumTextView = itemView.findViewById<TextView>(R.id.playersNum)
+        private var detailsButton = itemView.findViewById<Button>(R.id.details)
 
-        fun setDate(date: String?) {
-//            dateTextView.text = date ?: "no date"
+        init {
+            setOnClickListener()
+        }
+
+        private fun setOnClickListener() {
+            detailsButton.setOnClickListener {
+
+            }
         }
 
         fun setTotal(total: Int) {
             totalTextView.text = total.toString()
-        }
-
-        fun setPlace(place: Int?) {
-//            placeTextView.text = place?.toString() ?: "no place"
-        }
-
-        fun setPlayersCount(playersCount: Int?) {
-//            playersNumTextView.text = playersCount?.toString() ?: "no players count"
         }
     }
 }
